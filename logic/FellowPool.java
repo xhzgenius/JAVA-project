5eee96af83e02a3b46a86bf65078ac94cd4be0d6
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2022-06-02 18:37:54 +0800

    feat: fill default FellowPool

--- logic/FellowPool.java
+++ logic/FellowPool.java
@@ -9,7 +9,17 @@ import java.util.*;
 public class FellowPool
 {
     public ArrayList<Fellow> fellowList = new ArrayList<Fellow>();
-    public FellowPool(){}
+    public FellowPool(){
+        fellowList.add(new Alleycat());
+        fellowList.add(new IckyImp());
+        fellowList.add(new RockpoolHunter());
+        fellowList.add(new ImpulsiveTrickster());
+        fellowList.add(new Scallywag());
+        fellowList.add(new HarvestGolem());
+        fellowList.add(new Leapfrogger());
+        fellowList.add(new MetaltoothLeaper());
+        fellowList.add(new MenagerieMug());
+    }
     public void add(Fellow f)
     {
         fellowList.add(f);
