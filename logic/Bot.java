e65dff5515f535755dd9441ac9a76e66b83d4f01
xhzgenius <82707775+xhzgenius@users.noreply.github.com>
2022-06-07 13:02:01 +0800

    mqt的bot写完了
    mqt的bot写完了

--- logic/Bot.java
+++ logic/Bot.java
@@ -9,11 +9,24 @@ public class Bot {
     }
     void act() throws GameException
     {
-        // call getShowFellows and enroll the first one
-        System.out.println(String.format("[Bot] %d act", playerID));
-        System.out.println(game.getShowFellows(playerID).toString());
-        Fellow fellow = game.getShowFellows(playerID).get(0);
-        game.enroll(playerID, fellow);
-        game.cast(playerID, fellow, 0);
+        // 阿巴阿巴
+		int t = game.turn;
+		int botCoin = Math.min(3+t,10);
+		ArrayList<Fellow> botInventory = getInventory(playerID);
+		while(botCoin>=3&&botInventory.size()<10){
+			ArrayList<Fellow> ShowFellow = getShowFellows(playerID)；
+		    Fellow fm = ShowFellow.stream().max(Comparator.comparing(getLevel(playerID))).get();
+		    Store store = storesList.get(playerID);
+            store.enroll(fm);
+            boolean result = mergeFellow(fm, battlefieldsList.get(playerID), botInventory);
+            if(result==false) botInventory.add(fm);
+		}
+		ArrayList<Fellow> botList = getBattlefield(playerID);
+		while(botList.size()<7){
+		    int randNumber1 = rand.nextInt(botInventory.size());
+            Fellow f = botInventory.get(randNumber1);
+		    int randNumber2 = rand.nextInt(7) + 1;
+		    changePositionOrCast(playerID,f,randNumber2);
+		}
     }
 }
